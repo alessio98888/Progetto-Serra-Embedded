c93a517007b9d9e0b8abd755ebe0c4b453f91044
Riccardo Mescoli <64926868+RiccardoMescoli@users.noreply.github.com>
2021-11-01 00:44:54 +0100

    Removed unnecessary support variables used to temporarily store sensor readings

--- Progetto-Serra-Embedded.ino
+++ Progetto-Serra-Embedded.ino
@@ -1,7 +1,7 @@
 #include "DHT.h"
+
 // MISRA-C Compliance (size specific data types). ESP32 boards use 32 bit for storing floats
 typedef float float32_t;
-
 
 #if CONFIG_FREERTOS_UNICORE
   #define ARDUINO_RUNNING_CORE 0
@@ -14,13 +14,13 @@ typedef float float32_t;
 #endif
 
 /*--------------------------------------------------*/
-/*------------------ Pin Defines ------------------*/
+/*------------------ Pin Defines -------------------*/
 /*--------------------------------------------------*/
 #define DHT11PIN 4 // Digital temperature and air humidity sensor
 #define YL69PIN 22 // Analog soil humidity sensor
 
 /*--------------------------------------------------*/
-/*----------- Task Function Prototypes ------------*/
+/*----------- Task Function Prototypes -------------*/
 /*--------------------------------------------------*/
 void TaskCoordinator ( void *pvParameters );
 void TaskReadDHT11Temperature( void *pvParameters );
@@ -192,19 +192,16 @@ void TaskReadDHT11Temperature(void *pvParameters)
     Read temperature as Celsius (the default)
   */
   
-  float32_t temperature_reading;
   struct sensor_msg sensor_reading_struct;
   sensor_reading_struct.sensor = Sensor_Id_DHT11Temperature;
   
   for (;;) 
   {
     #ifdef DEBUG
-      temperature_reading = sensorSim(); // DEBUG: sensor simulation
+      sensor_reading_struct.sensor_reading = sensorSim(); // DEBUG: sensor simulation
     #else
-      temperature_reading = dht.readTemperature(); // actual sensor reading
+      sensor_reading_struct.sensor_reading = dht.readTemperature(); // actual sensor reading
     #endif
-    
-    sensor_reading_struct.sensor_reading = temperature_reading;
     
     if( xQueueSend ( coordinator_queue , 
                     (void *) &sensor_reading_struct , 
@@ -231,19 +228,16 @@ void TaskReadDHT11Humidity(void *pvParameters)
     So, if the humidity is 60 per cent(which is the average humidity), then 60 per cent of the air around you is water vapor
   */
   
-  float32_t humidity_reading;
   struct sensor_msg sensor_reading_struct;
   sensor_reading_struct.sensor = Sensor_Id_DHT11Humidity;
   
   for (;;) 
   {
     #ifdef DEBUG
-      humidity_reading = sensorSim(); // DEBUG: sensor simulation
+      sensor_reading_struct.sensor_reading = sensorSim(); // DEBUG: sensor simulation
     #else
-      humidity_reading = dht.readHumidity(); // actual sensor reading
+      sensor_reading_struct.sensor_reading = dht.readHumidity(); // actual sensor reading
     #endif
-    
-    sensor_reading_struct.sensor_reading = humidity_reading;
     
     if( xQueueSend ( coordinator_queue , 
                     (void *) &sensor_reading_struct , 
@@ -269,19 +263,16 @@ void TaskReadYL69SoilHumidity(void *pvParameters)
     Reads *soil* humidity in per cent.
   */
   
-  float32_t soil_humidity_reading;
   struct sensor_msg sensor_reading_struct;
   sensor_reading_struct.sensor = Sensor_Id_YL69SoilHumidity;
   
   for (;;) 
   {
     #ifdef DEBUG
-      soil_humidity_reading = sensorSim(); // DEBUG: sensor simulation
+      sensor_reading_struct.sensor_reading = sensorSim(); // DEBUG: sensor simulation
     #else
-      soil_humidity_reading = map(analogRead(YL69PIN), 1023, 0, 0, 100);  // actual sensor reading
+      sensor_reading_struct.sensor_reading = map(analogRead(YL69PIN), 1023, 0, 0, 100);  // actual sensor reading
     #endif
-    
-    sensor_reading_struct.sensor_reading = soil_humidity_reading;
     
     if( xQueueSend ( coordinator_queue , 
                     (void *) &sensor_reading_struct , 
